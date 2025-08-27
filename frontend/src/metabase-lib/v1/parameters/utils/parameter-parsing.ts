b84ccf9f9a44563e5371f9fe27c77e62d5f58cec
Romeo Van Snick <romeo@romeovansnick.be>
2025-08-27 21:23:01 +1000

    Use ParameterValuesMap where possible

--- frontend/src/metabase-lib/v1/parameters/utils/parameter-parsing.ts
+++ frontend/src/metabase-lib/v1/parameters/utils/parameter-parsing.ts
@@ -17,6 +17,7 @@ import type {
   ParameterId,
   ParameterValue,
   ParameterValueOrArray,
+  ParameterValuesMap,
 } from "metabase-types/api";
 
 export function getParameterValueFromQueryParams(
@@ -147,8 +148,8 @@ export function getParameterValuesByIdFromQueryParams(
   parameters: Parameter[],
   queryParams: Query,
   lastUsedParametersValues?: Record<ParameterId, unknown>,
-): Record<ParameterId, ParameterValueOrArray | null> {
-  const result: Record<ParameterId, ParameterValueOrArray | null> = {};
+): ParameterValuesMap {
+  const result: ParameterValuesMap = {};
   for (const parameter of parameters) {
     result[parameter.id] = getParameterValueFromQueryParams(
       parameter,
